ea33e98ac774906bb1152c52ffba49b950797d3c
kesha2k17 <kenedydushime@gmail.com>
2022-12-12 19:05:18 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include	<stdio.h>
+#include	<stdlib.h>
+/**
+ * main	-	Entry	point
+ *
+ * Return:	Always	0	(Success)
+ */
+int	main(void)
+{
+	int n;
+
+	while (n < 10)
+	{
+		printf("%d", n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}

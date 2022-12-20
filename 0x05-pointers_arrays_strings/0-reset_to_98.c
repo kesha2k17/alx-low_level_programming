fea9f73d32136f43d93b52373371224255ac1642
kesha2k17 <kenedydushime@gmail.com>
2022-12-19 17:14:58 -0800

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,9 +3,8 @@
 /**
  * reset_to_98 - function that takes a pointer to an integer parameter
  * @n: integer pointer
- * Return: 0 Success
+ *  Return: 0 Success
  */
-
 void reset_to_98(int *n)
 
 {
